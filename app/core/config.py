55602d38fb4224e09e7406b9e116313152db5326
Dillon Montag <dillon.montag@divido.com>
2022-02-19 17:49:44 +0000

    fix: remove trailing slash from translator api

--- app/core/config.py
+++ app/core/config.py
@@ -6,7 +6,7 @@ class Settings(BaseSettings):
 
     API_V1_STR: str = "/api/v1"
     POKE_API_URL: str = "https://pokeapi.co/api/v2"
-    TRANSLATOR_API_URL: str = "https://api.funtranslations.com/translate/"
+    TRANSLATOR_API_URL: str = "https://api.funtranslations.com/translate"
 
     class Config:
         case_sensitive = True # match env vars based on case
