2e316f2a6e890c83d01047b4f7f104233f0f6967
Dillon Montag <dillon.montag@divido.com>
2022-02-19 17:48:09 +0000

    chore: add variables for external apis

--- app/core/config.py
+++ app/core/config.py
@@ -3,7 +3,10 @@ from pydantic import BaseSettings
 
 # The Pydantic BaseSettings class tries to populate the values of these variables from env variables first
 class Settings(BaseSettings):
+
     API_V1_STR: str = "/api/v1"
+    POKE_API_URL: str = "https://pokeapi.co/api/v2"
+    TRANSLATOR_API_URL: str = "https://api.funtranslations.com/translate/"
 
     class Config:
         case_sensitive = True # match env vars based on case
