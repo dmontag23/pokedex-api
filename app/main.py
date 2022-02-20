6cb3ca64e604003f5e26afa40d2376ad1b34e2a6
Dillon Montag <dillon.montag@divido.com>
2022-02-20 18:55:00 +0000

    feat: add health endpoint

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from logging.config import dictConfig
 from fastapi import FastAPI
 
 from app.api.api_v1.api import api_router
+from app.schemas.health import Health
 from app.schemas.log_config import LogConfig
 
 dictConfig(LogConfig().dict())  # setup logging config
@@ -15,3 +16,9 @@ app = FastAPI(title="Pokedex API")
 # but this does not conform with the original design specs
 # app.include_router(api_router, prefix=settings.API_V1_STR)
 app.include_router(api_router)
+
+
+# the health endpoint is here because it will never be versioned
+@app.get("/health", tags=["health"], response_model=Health)
+def health():
+    return {"status": "ok"}
---
+++ app/tests/test_health.py
@@ -0,0 +1,11 @@
+import pytest
+from httpx import Response
+
+
+@pytest.mark.asyncio
+async def test_health(async_app_client, respx_mock) -> None:
+    """Test the health endpoint is working as expected"""
+
+    response: Response = await async_app_client.get("/health")
+    assert response.status_code == 200
+    assert response.json().get("status") == "ok"
