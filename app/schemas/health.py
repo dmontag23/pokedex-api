007430f9f2fcc29e656ae5287e699f99126e225b
Dillon Montag <dillon.montag@divido.com>
2022-02-20 19:05:53 +0000

    feat: update response schemas with examples

--- app/schemas/health.py
+++ app/schemas/health.py
@@ -3,3 +3,10 @@ from pydantic import BaseModel
 
 class Health(BaseModel):
     status: str
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "status": "ok",
+            }
+        }
--- app/schemas/pokemon.py
+++ app/schemas/pokemon.py
@@ -6,3 +6,13 @@ class Pokemon(BaseModel):
     description: str
     habitat: str
     isLegendary: str
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "name": "mewtwo",
+                "description": "I am mewtwo hear me roar!",
+                "habitat": "Someone's mancave",
+                "isLegendary": "true"
+            }
+        }
