60346e5ea3bb53d9a69bf82cbce9128234434340
Dillon Montag <dillon.montag@divido.com>
2022-02-20 14:02:41 +0000

    chore: add typing and docstring to test client generator

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -1,3 +1,5 @@
+from typing import Generator
+
 import pytest_asyncio
 from httpx import AsyncClient
 
@@ -5,6 +7,7 @@ from app.main import app
 
 
 @pytest_asyncio.fixture
-async def async_app_client():
+async def async_app_client() -> Generator[AsyncClient, None, None]:
+    """Create a client to make requests to the api"""
     async with AsyncClient(app=app, base_url='http://test') as client:
         yield client
