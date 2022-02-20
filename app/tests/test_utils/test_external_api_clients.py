d64cfddf57d4b24b572592eb84a564b092edc798
Dillon Montag <dillon.montag@divido.com>
2022-02-20 16:35:45 +0000

    feat: add exception handling

--- app/tests/test_utils/test_external_api_clients.py
+++ app/tests/test_utils/test_external_api_clients.py
@@ -1,5 +1,6 @@
 import pytest
 import respx
+from fastapi import HTTPException
 from httpx import Response
 
 from app.core.config import settings
@@ -34,8 +35,32 @@ async def test_poke_api_client_success(respx_mock) -> None:
 
 
 @pytest.mark.asyncio
+async def test_poke_api_client_fails(respx_mock) -> None:
+    """Test that the PokeAPIClient raises an exception when an error occurs"""
+
+    # setup mock url
+    url_to_mock = f"{settings.POKE_API_URL}/pokemon-species/mewtwo"
+    mocked_poke_api_get_route = respx_mock.get(url_to_mock).mock(
+        side_effect=HTTPException(status_code=404, detail="Not Found")
+    )
+
+    # call the client
+    with pytest.raises(HTTPException) as err:
+        await PokeAPIClient.send_request("pokemon-species/mewtwo")
+
+    request, response = respx.calls.last
+    assert mocked_poke_api_get_route.called
+    assert mocked_poke_api_get_route.call_count == 1
+    assert request.url == url_to_mock
+    assert response is None
+    assert err.value.status_code == 404
+    assert err.value.detail == "Not Found"
+
+
+@pytest.mark.asyncio
 async def test_fun_translations_api_client_success(respx_mock) -> None:
     """Test that the FunTranslationsAPIClient is working as expected"""
+
     # setup mock url
     url_to_mock = f"{settings.FUN_TRANSLATIONS_API_URL}/yoda"
     mocked_fun_translations_api_post_route = respx_mock.post(url_to_mock).mock(
@@ -55,3 +80,26 @@ async def test_fun_translations_api_client_success(respx_mock) -> None:
            in request.content.decode('utf-8').replace("+", " ")
     assert response.status_code == 200
     assert response.json() == MEWTWO_YODA_JSON_RESPONSE
+
+
+@pytest.mark.asyncio
+async def test_fun_translations_api_client_fails(respx_mock) -> None:
+    """Test that the FunTranslationsAPIClient raises an exception when an error occurs"""
+
+    # setup mock url
+    url_to_mock = f"{settings.FUN_TRANSLATIONS_API_URL}/yoda"
+    mocked_poke_api_get_route = respx_mock.post(url_to_mock).mock(
+        side_effect=HTTPException(status_code=404, detail="Not Found")
+    )
+
+    # call the client
+    with pytest.raises(HTTPException) as err:
+        await FunTranslationsAPIClient.send_request("yoda", payload=MEWTWO_YODA_JSON_REQUEST)
+
+    request, response = respx.calls.last
+    assert mocked_poke_api_get_route.called
+    assert mocked_poke_api_get_route.call_count == 1
+    assert request.url == url_to_mock
+    assert response is None
+    assert err.value.status_code == 404
+    assert err.value.detail == "Not Found"
--- app/utils/external_api_clients.py
+++ app/utils/external_api_clients.py
@@ -1,6 +1,7 @@
-from typing import Any
+import json
 
-import httpx
+from fastapi import HTTPException
+from httpx import AsyncClient, Response
 from pydantic import Json
 
 from app.core.config import settings
@@ -11,20 +12,50 @@ class PokeAPIClient:
     base_url = settings.POKE_API_URL
 
     @classmethod
-    async def send_request(cls, endpoint: str) -> Any:
-        async with httpx.AsyncClient(base_url=cls.base_url) as client:
+    async def send_request(cls, endpoint: str) -> Response:
+        async with AsyncClient(base_url=cls.base_url) as client:
             logger.info("Attempting to get pokemon data from "
                         f"{cls.base_url}/{endpoint}")
-            return await client.get(f"/{endpoint}")
+
+            response: Response = await client.get(f"/{endpoint}")
+
+            if not response.is_success:
+                logger.error(f"Error calling {response.url}: "
+                             f"Response is {response.text}")
+                raise HTTPException(
+                    status_code=response.status_code,
+                    detail=f"Error when calling {response.url}: "
+                           f"{response.text}"
+                )
+
+            return response
 
 
 class FunTranslationsAPIClient:
     base_url = settings.FUN_TRANSLATIONS_API_URL
 
     @classmethod
-    async def send_request(cls, endpoint: str, payload: Json) -> Any:
-        async with httpx.AsyncClient(base_url=cls.base_url) as client:
+    async def send_request(cls, endpoint: str, payload: Json) -> Response:
+        async with AsyncClient(base_url=cls.base_url) as client:
             logger.info("Attempting to get translation data from "
                         f"{cls.base_url}/{endpoint}")
             logger.debug(f"The payload provided is {payload}")
-            return await client.post(f"/{endpoint}", data=payload)
+
+            response: Response = await client.post(f"/{endpoint}", data=payload)
+
+            if not response.is_success:
+                logger.error(f"Error calling {response.url}: "
+                             f"Response is {response.text}")
+                try:
+                    error_message = json.loads(response.text)
+                    error_message = error_message.get('error').get('message')
+                except ValueError:
+                    logger.info("Could not convert error from external api to json")
+                    error_message = response.text
+                raise HTTPException(
+                    status_code=response.status_code,
+                    detail=f"Error when calling {response.url}: "
+                           f"{error_message}"
+                )
+
+            return response
