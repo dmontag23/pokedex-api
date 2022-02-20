69a6a775b697875c7f5015430cb5b5ade4fffbaa
Dillon Montag <dillon.montag@divido.com>
2022-02-20 18:16:37 +0000

    chore: further refactoring of custom client

--- app/utils/external_api_clients.py
+++ app/utils/external_api_clients.py
@@ -7,38 +7,49 @@ from app.core.logger import logger
 
 
 async def send_request(
-        url: str,
+        url: str,  # noqa
         method: str,
-        payload:
-        dict[str, str] = {}
+        payload: dict[str, str] = {}
 ) -> Response:
-    async with AsyncClient() as client:
-        logger.info(f"Attempting to {method} data:  "
-                    f"{url}")
+    """
+    Implements a custom client wrapper to make
+    external HTTP requests
+
+    There are a lot of improvements that could be made:
+    - Handling errors from external apis could make use
+      of respective api error schemas
+    - The response from the external api could be cached
+    """
+
+    async with AsyncClient() as client:  # noqa
         logger.debug(f"The payload provided is {payload}")
 
-        match method:
-            case "GET":
-                response: Response = await client.get(
-                    f"{url}",
-                )
-            case "POST":
-                response: Response = await client.post(
-                    f"{url}",
-                    data=payload
-                )
-            case _:
-                raise HTTPException(
-                    status_code=status.HTTP_405_METHOD_NOT_ALLOWED,
-                    detail=f"The client does not yet support the"
-                           f" {method} method"
-                )
+        # check currently implemented methods
+        implemented_methods = ["GET", "POST"]
+        if method not in implemented_methods:
+            logger.error(f"Method {method} is not currently implemented")
+            raise HTTPException(
+                status_code=status.HTTP_405_METHOD_NOT_ALLOWED,
+                detail=f"The client does not yet support the"
+                       f" {method} method"
+            )
 
+        # build client call - ideally would also store response in cache
+        suffix = ")" if method == "GET" else ", data=payload)"
+        response: Response = await eval(
+            f"client.{method.lower()}(url{suffix}"
+        )
+
+        # handle errors - this section could be refactored
+        # to better handle any errors that come back from the
+        # client by utilizing the client error schema(s)
         if not response.is_success:
             logger.error(f"Error calling {response.url}: "
                          f"Response is {response.text}")
             try:
                 error_message = json.loads(response.text)
+                logger.info("Json converted error message "
+                            f"is {error_message}")
                 error_message = error_message.get('error').get('message')
             except ValueError:
                 logger.info("Could not convert error "
